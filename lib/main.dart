e5a872fb1f69bc9bbf84098b2a56551430ceb4ff
NataleCaoTuong <ltzntl83a28b666j@studenti.unime.it>
2021-05-26 22:49:01 +0200

    Added login and signup

--- lib/main.dart
+++ lib/main.dart
@@ -12,26 +12,70 @@ import 'package:osm_reports/icons.dart';
 import 'package:popup_menu/popup_menu.dart';
 import 'package:zoom_widget/zoom_widget.dart';
 import 'custom_popup.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'dart:convert' show json, base64, ascii;
 
 LatLng _center = LatLng(38.19394, 15.55256);
+const SERVER_IP = 'http://10.0.2.2:3000';
+final storage = FlutterSecureStorage();
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  Future<String> get jwtOrEmpty async {
+    var jwt = await storage.read(key: "jwt");
+    if (jwt == null) return "";
+    return jwt;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'OSM reports',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: FutureBuilder(
+          future: jwtOrEmpty,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) return CircularProgressIndicator();
+            if (snapshot.data != "") {
+              var str = snapshot.data;
+              var jwt = str.split(".");
+
+              if (jwt.length != 3) {
+                return LoginPage();
+              } else {
+                var payload = json.decode(
+                    ascii.decode(base64.decode(base64.normalize(jwt[1]))));
+                if (DateTime.fromMillisecondsSinceEpoch(payload["exp"] * 1000)
+                    .isAfter(DateTime.now())) {
+                  return HomePage(str, payload);
+                } else {
+                  return LoginPage();
+                }
+              }
+            } else {
+              return LoginPage();
+            }
+          }),
     );
   }
 }
 
 class HomePage extends StatefulWidget {
+  final String jwt;
+  final Map<String, dynamic> payload;
+
+  factory HomePage.fromBase64(String jwt) => HomePage(
+      jwt,
+      json.decode(
+          ascii.decode(base64.decode(base64.normalize(jwt.split(".")[1])))));
+
+  const HomePage(this.jwt, this.payload);
+
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -234,6 +278,22 @@ class _HomePageState extends State<HomePage> {
           },
         ),
         body: Column(children: <Widget>[
+          FutureBuilder(
+              future: http.read('$SERVER_IP/data',
+                  headers: {"Authorization": widget.jwt}),
+              builder: (context, snapshot) => snapshot.hasData
+                  ? Column(
+                      children: <Widget>[
+                        Text("Welcome " +
+                            widget.payload[
+                                'username']) /*,
+                        Text(snapshot.data,
+                            style: Theme.of(context).textTheme.headline4)*/
+                      ],
+                    )
+                  : snapshot.hasError
+                      ? Text("An error occurred")
+                      : CircularProgressIndicator()),
           ElevatedButton(
             autofocus: false,
             clipBehavior: Clip.none,
@@ -241,7 +301,7 @@ class _HomePageState extends State<HomePage> {
             child: Text('Select type of report'),
           ),
           SizedBox(
-              height: 550,
+              height: 500,
               child: Zoom(
                   height: 1800,
                   width: 1800,
@@ -370,5 +430,98 @@ class _HomePageState extends State<HomePage> {
           child: get_icon(marker_icon_value[i])),
       opacity: infoWindowVisible ? 0.0 : 1.0,
     );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void displayDialog(context, title, text) => showDialog(
+        context: context,
+        builder: (context) =>
+            AlertDialog(title: Text(title), content: Text(text)),
+      );
+
+  Future<String> attemptLogIn(String username, String password) async {
+    var res = await http.post("$SERVER_IP/login",
+        body: {"username": username, "password": password});
+    if (res.statusCode == 200) return res.body;
+    return null;
+  }
+
+  Future<int> attemptSignUp(String username, String password) async {
+    var res = await http.post('$SERVER_IP/signup',
+        body: {"username": username, "password": password});
+    return res.statusCode;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Log In"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: _usernameController,
+                decoration: InputDecoration(labelText: 'Username'),
+              ),
+              TextField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(labelText: 'Password'),
+              ),
+              TextButton(
+                  onPressed: () async {
+                    var username = _usernameController.text;
+                    var password = _passwordController.text;
+                    var jwt = await attemptLogIn(username, password);
+                    if (jwt != null) {
+                      storage.write(key: "jwt", value: jwt);
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => HomePage.fromBase64(jwt)));
+                    } else {
+                      displayDialog(context, "An Error Occurred",
+                          "No account was found matching that username and password");
+                    }
+                  },
+                  child: Text("Log In")),
+              TextButton(
+                  onPressed: () async {
+                    var username = _usernameController.text;
+                    var password = _passwordController.text;
+
+                    if (username.length < 4)
+                      displayDialog(context, "Invalid Username",
+                          "The username should be at least 4 characters long");
+                    else if (password.length < 4)
+                      displayDialog(context, "Invalid Password",
+                          "The password should be at least 4 characters long");
+                    else {
+                      var res = await attemptSignUp(username, password);
+                      if (res == 201)
+                        displayDialog(context, "Success",
+                            "The user was created. Log in now.");
+                      else if (res == 409)
+                        displayDialog(
+                            context,
+                            "That username is already registered",
+                            "Please try to sign up using another username or log in if you already have an account.");
+                      else {
+                        displayDialog(
+                            context, "Error", "An unknown error occurred.");
+                      }
+                    }
+                  },
+                  child: Text("Sign Up"))
+            ],
+          ),
+        ));
   }
 }
